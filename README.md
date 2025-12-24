9ba3973500fdfb4c0da28a4f472a40d6e96dc7a3
koshiq <koshiqhossain@gmail.com>
2025-12-24 00:16:14 -0500

    matmul

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 | **Day**    | **Daily Summary**                                                                      |
 |------------|----------------------------------------------------------------------------------------|
 | Day 1      |  CUDA set up, kernel that prints "Hello World", CUDA-puzzle1-Map                       |
-| Day 2      |  Set up new 5070ti GPU, simple vector_add.cu                                           |
+| Day 2      |  simple & unified memory vectorAdd.cu and simple matMul.c                              |
---
+++ day2/matMul/matMul.cu
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+__global__ void matMUL(int *a, int *b, int *c, int n){
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < n && col < n) {
+        c[row * n + col] = 0;
+        for (int k = 0; k < n; k++) {
+            c[row * n + col] += a[row * n + k] * b[k * n + col];
+        }
+    }
+}
+
+
+void init_matrices(int *a, int *b, int *c, int n) {
+    for (int i = 0; i < n * n; i++) {
+        a[i] = rand() % 100;
+        b[i] = rand() % 100;
+        c[i] = 0;
+    }
+}
+
+void check_answer(int *a, int *b, int *c, int n) {
+    int *verify_c;
+    verify_c = (int*)malloc(n * n * sizeof(int));
+    for (int i = 0; i < n * n; i++) {
+        verify_c[i] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                verify_c[i * n + j] += a[i * n + k] * b[k * n + j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n * n; i++) {
+        assert(c[i] == verify_c[i]);
+    }
+    free(verify_c);
+}
+
+int main() {
+    int n = 1 << 10;
+    size_t bytes = sizeof(int) * n * n;
+
+    int *h_a, *h_b, *h_c;
+    h_a = (int*)malloc(bytes);
+    h_b = (int*)malloc(bytes);
+    h_c = (int*)malloc(bytes);
+
+    int *d_a, *d_b, *d_c;
+    cudaMalloc((void**)&d_a, bytes);
+    cudaMalloc((void**)&d_b, bytes);
+    cudaMalloc((void**)&d_c, bytes);
+
+    init_matrices(h_a, h_b, h_c, n);
+
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
+    
+    int BLOCK_SIZE = 16;
+    int GRID_SIZE = (int)ceil(n / BLOCK_SIZE);
+    
+    dim3 grid(GRID_SIZE, GRID_SIZE);
+    dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+
+    matMUL<<<grid, threads>>>(d_a, d_b, d_c, n);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost);
+    check_answer(h_a, h_b, h_c, n);
+
+    printf("Matrix multiplication completed successfully.\n");
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    free(h_a);
+    free(h_b);
+    free(h_c);
+
+    return 0;
+}
